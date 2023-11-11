be8e1fd4a5a1196bb469bb5a5b25cf051d04e81b
ashhadulislam <fa1800791@qu.edu.qa>
2023-11-11 19:02:49 +0300

    all other files

---
+++ stream_app.py
@@ -0,0 +1,83 @@
+# https://github.com/wjbmattingly/streamlit_lessons_youtube/blob/main/01_intro_to_streamlit_app.py
+
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+import json
+import pickle
+
+
+file = open("dic_data.obj",'rb')
+dic_data = pickle.load(file)
+file.close()
+
+
+file = open("dic_companywise.obj",'rb')
+dic_companywise = pickle.load(file)
+file.close()
+
+
+
+st.title("Boycott for palestine")
+st.header("Details about brands to boycott")
+# button1 = st.button("Click Me")
+# if button1:
+#     st.write("This is some text.")
+
+# st.header("Start of the Checkbox Section")
+# like = st.checkbox("Do you like this app?")
+# button2 = st.button("Submit")
+# if button2:
+#     if like:
+#         st.write("Thanks. I like it too.")
+#     else:
+#         st.write("I'm sorry. You have bad tastes.")
+
+# st.header("Start of the Radio Button Section")
+# animal = st.radio("What animal is your favorite?", ("Lion", "Tiger", "Bear"))
+# button3 = st.button("Submit Animal")
+# if button3:
+#     st.write(animal)
+#     if animal == "Lion":
+#         st.write("ROAR!")
+
+categories=list(dic_data.keys())
+category = st.selectbox("Choose category?", categories)
+
+print("Chosen category is",category)
+
+companies=dic_data[category]['names']
+
+name = st.selectbox("Choose Company?", companies)
+
+print("Chosen company is",name)
+
+
+company_details = dic_companywise[name]
+st.image(company_details["logo"],width=200)
+
+print("Chosen company_details is",company_details)
+
+# st.subheader("Description")
+st.text(company_details["description"])
+
+
+st.text("Reason For Boycott")
+st.caption(company_details["reason"])
+
+
+st.text("Source Of Information")
+st.caption(company_details["source"])
+st.markdown("""---""")
+
+
+st.text("Action")
+st.caption(company_details["howToBoycott"])
+
+
+
+st.text("Alternatives")
+st.caption(company_details["alternatives"])
+st.markdown("""---""")
+
+
